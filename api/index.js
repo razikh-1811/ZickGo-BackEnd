335e4a20df0d045938286d9a464517201a18aab1
Shaik Mahaboob Razikh <skrazikh1811@gmail.com>
2025-10-16 10:59:29 +0530

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import serverless from "serverless-http";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: [
+    "https://your-frontend.vercel.app",
+    "https://your-admin.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+app.use(express.json());
+
+//  Connect MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected "))
+  .catch(err => console.log("Mongo error ", err));
+
+// Test route
+app.get("/api", (req, res) => {
+  res.send("🚀 Backend running on Vercel!");
+});
+
+//  Export for Vercel
+export const handler = serverless(app);
+export default handler;
